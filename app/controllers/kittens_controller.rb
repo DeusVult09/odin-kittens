1290a30c1e52e5545f542b5709f5a94882db9cc1
Zarifa <mamadalievazara@gmail.com>
2026-08-02 15:31:50 +0200

    Add flash notice and alerts and render in the application.html

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -11,8 +11,9 @@ class KittensController < ApplicationController
   def create
     @kitten = Kitten.new(kitten_params)
     if @kitten.save
-      redirect_to @kitten
+      redirect_to @kitten, notice: "Majestic Kitten was Created!"
     else
+      flash.now[:alert] = "Somebody Stole the Kitten"
       render :new, status: :unprocessable_entity
     end
   end
@@ -30,8 +31,8 @@ class KittensController < ApplicationController
   
   def destroy
     @kitten.destroy
-
-    redirect_to @kitten, notice: "Kitten was successfully deleted"
+    
+    redirect_to @kitten, notice: "Kitten was Unfortunately deleted"
   end
 
   private
